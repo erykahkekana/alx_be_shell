cd5178300aef371b5ff76b17e003e390db2cb617
Mogau Ramara <kekanaerykah@gmail.com>
2024-12-17 13:13:21 +0200

    Create a Python script named weather_advice.py. This script will ask the user about the current weather conditions and provide clothing recommendations based on the input. This task aims to demonstrate the use of if, elif, and else statements to make decisions in a program.

---
+++ control_flow/weather_advice.py
@@ -0,0 +1,10 @@
+ask_weather = input("What's thr weather like today? (sunny/rainy/cold):  ")
+
+if ask_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif ask_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif ask_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
